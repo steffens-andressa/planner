581b38c4dd68067022d9c6e1d4f75406f401ca8a
Andressa Schaff Steffens <steffensand@gmail.com>
2024-07-08 23:24:18 -0300

    initial commit

---
+++ src/test/java/com/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
